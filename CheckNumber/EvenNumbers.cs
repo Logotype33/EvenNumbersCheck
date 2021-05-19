0f031c667738897657acad9ab5acd28ede7616be
ssss FFFFF <logotip1010@mail.ru>
2021-05-19 17:05:59 +0300

    Добавьте файлы проекта.

---
+++ CheckNumber/EvenNumbers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckNumber
+{
+    public class EvenNumbers
+    {
+        public bool CheckForEvenNumber(int number) => number % 2 == 0 ? true : false;
+        public int[] GetEvenNumbers(int[] numbers)=> numbers.Where(x => x % 2 == 0).ToArray();
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,19 @@
+﻿using CheckNumber;
+using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EvenNumbers check = new EvenNumbers();
+            Console.WriteLine(check.CheckForEvenNumber(5));
+            var evenNumbs= check.GetEvenNumbers(new int[] { 1, 2, 3, 5, 6 });
+            foreach (var item in evenNumbs)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
